4c293196a86ec75423d5bf8774adabf94273ef9e
Italo Menezes <italomlporoca@hotmail.com>
2020-09-19 22:18:21 -0300

    List favorites on favorites screen

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { View } from 'react-native';
-import { Header } from '../../components';
-
-// import { Container } from './styles';
+import { useSelector } from 'react-redux';
+import { Header, MoviesList } from '../../components';
+import { RootReducer } from '../../store/modules/rootReducer';
 
 const Favorites: React.FC = () => {
+  const favorites = useSelector((state: RootReducer) => state.favorites.movies);
+
   return (
     <View>
-      <Header title="Favoritos" />
+      <Header title="Favoritos" description="Veja aqui seus filmes favoritos" />
+      <MoviesList
+        movies={favorites}
+        emptyText="Não há filmes favoritos ainda. Adicione favoritos na página de busca."
+      />
     </View>
   );
 };
